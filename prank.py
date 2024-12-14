4029fe06d2f2c0b86d59db8c58db487f57ded347
Anonymus2222222 <siphonrigolomathias@gmail.com>
2024-12-14 11:55:07 +0100

    Add files via upload

---
+++ prank.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prank Sonore</title>
+</head>
+<body>
+    <h1>Surprise !</h1>
+    <audio autoplay>
+        <source src="https://www.myinstants.com/media/sounds/fart.mp3" type="audio/mpeg">
+        Votre navigateur ne supporte pas l'audio.
+    </audio>
+</body>
+</html>
